7651a19703f8a4fb3cfe94c4e45e188648e8f763
seabbs <s.e.abbott12@gmail.com>
2020-10-11 10:57:16 +0000

    update to latest interface

--- R/update-rt.R
+++ R/update-rt.R
@@ -7,14 +7,6 @@ library(here, quietly = TRUE)
 # Set target date ---------------------------------------------------------
 target_date <- as.character(Sys.Date())
 
-# Set up logging ----------------------------------------------------------
-setup_logging("INFO", file = paste0("logs/summary/", target_date, ".log"),
-              mirror_to_console = TRUE)
-setup_logging("INFO", file = paste0("logs/detailed/", target_date, ".log"),
-              name = "EpiNow2.epinow")
-setup_logging("INFO", file = paste0("logs/fit/", target_date, ".log"),
-              name = "EpiNow2.epinow.fit")
-
 # Update delays -----------------------------------------------------------
 generation_time <- readRDS(here::here("data", "delays", "generation_time.rds"))
 incubation_period <- readRDS(here::here("data", "delays", "incubation_period.rds"))
@@ -30,18 +22,18 @@ data.table::setorder(cases, region, date)
 plan("multiprocess", gc = TRUE, earlySignal = TRUE)
 
 # Run Rt estimation -------------------------------------------------------
-regional_epinow(reported_cases = cases, method = "approximate",
+regional_epinow(reported_cases = cases, 
+                method = "approximate",
                 generation_time = generation_time, 
                 delays = list(incubation_period, reporting_delay),
-                stan_args = list(trials = 5), horizon = 7, samples = 2000, 
-                burn_in = 14, fixed_future_rt = TRUE, summary = FALSE,
+                stan_args = list(trials = 5), 
+                samples = 2000, 
+                horizon = 7, 
+                burn_in = 14, 
+                output = c("region", "summary", "timing"),
+                fixed_future_rt = TRUE, 
                 target_folder = here::here("data", "rt-samples"), 
-                return_estimates = FALSE,  max_execution_time = 60 * 20,
-                keep_samples = FALSE, make_plots = FALSE)
-
-# Summarise results -------------------------------------------------------
-regional_summary(reported_cases = cases,
-                 results_dir = here::here("data", "rt-samples"),
-                 summary_dir = here::here("data", "rt", target_date),
-                 all_regions = FALSE, return_summary = FALSE)
+                summary_args = list(summary_dir = here::here("data", "rt", target_date),
+                                    all_regions = FALSE),
+                max_execution_time = 60 * 20)
 
